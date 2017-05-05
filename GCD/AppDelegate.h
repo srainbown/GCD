13fbbe1aed96eec8c92e395445b609e38a9cb617
紫川秀 <tszy111@126.com>
2017-05-05 10:50:06 +0800

    Initial Commit

---
+++ GCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD
+//
+//  Created by 李自杨 on 2017/5/5.
+//  Copyright © 2017年 View. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD
+//
+//  Created by 李自杨 on 2017/5/5.
+//  Copyright © 2017年 View. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
